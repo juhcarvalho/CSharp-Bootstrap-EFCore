a9a829d5aab2aaa08f93832a091297d6670803ff
juliana.carvalho <jucarvalho@outlook.com.br>
2022-07-09 16:59:40 -0300

    Componente habitos e ajustes no layout

---
+++ Controle_de_Habito/Controllers/HabitoController.cs
@@ -0,0 +1,84 @@
+﻿using Controle_de_Habito.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controle_de_Habito.Controllers
+{
+    public class HabitoController : Controller
+    {
+        // GET: Habito
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Habito/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Habito/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Habito/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Habito habito)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Habito/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Habito/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Habito habito)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Habito/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Habito/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Habito habito)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controle_de_Habito/Models/Habito.cs
@@ -0,0 +1,16 @@
+﻿namespace Controle_de_Habito.Models
+{
+    public class Habito
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Objetivo { get; set; }
+
+        public string Dias { get; set; }
+
+        public DateTime Horario { get; set; }
+
+        public DateTime? DataInicio { get; set; }
+
+    }
+}
