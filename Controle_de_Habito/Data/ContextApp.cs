af6f1b87856b3bb7ea570bfa6cbf60d516394ddc
juliana.carvalho <jucarvalho@outlook.com.br>
2022-07-09 19:00:47 -0300

    Componente Listagem de hábitos, layout e Context.

---
+++ Controle_de_Habito/Data/ContextApp.cs
@@ -0,0 +1,23 @@
+﻿using Controle_de_Habito.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Controle_de_Habito.Data
+{
+    public class ContextApp : DbContext
+    {
+        public ContextApp()
+        {
+                
+        }
+
+        public virtual DbSet<Habito> Habito { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Properties<string>().Configure(p => p.HasColumnType("varchar"));
+            modelBuilder.Properties().Where(p => p.Name == "Id").Configure(p => p.IsKey());
+        }
+    }
+}
